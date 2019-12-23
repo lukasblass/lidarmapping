a0615d443bbbca29c95cd76bb8be7f3cac6d1266
lukas <blassl@student.ethz.ch>
2019-12-23 07:56:37 +0100

    added eigen

--- Lidar/include/Lidar/Lidar.h
+++ Lidar/include/Lidar/Lidar.h
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <vector>
+#include <Eigen/Dense>
+
 
 #define PI 3.14159265359
 
@@ -12,9 +14,17 @@ struct Measurement {
 
 class Lidar {
     public: 
-    Lidar(int measurements_) : measurements_per_cycle(measurements_) {}
+    Lidar(int measurements_) : measurements_per_cycle(measurements_) {
+        for (int i = 0; i<3; i++) {
+            for (int j = 0; j<3; j++) {
+                matrix(i,j) = i + j;
+            }
+        }
+    }
 
     std::vector<Measurement> scanACircle(const int radius);
+    
+    Eigen::Matrix3d matrix;
 
     private:
     int measurements_per_cycle = 360;
--- main/main.cpp
+++ main/main.cpp
@@ -10,5 +10,11 @@ int main() {
     for (const Measurement m : ms) {
         std::cout << m.distance << " " << m.angle << std::endl;
     }
+
+    for (int i = 0; i<3; i++) {
+        for (int j = 0; j<3; j++) {
+            std::cout << lidar.matrix(i,j) << std::endl;
+        }
+    }
     //std::cout << "hello " << std::endl;
 }
