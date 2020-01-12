2a9f76860ceed5ca7eeb52e4045c1ea42b54061d
lukasblass <lukas-blass@hotmail.com>
2020-01-12 15:33:47 +0100

    introduced randomness

--- Robot/include/Robot/RoboCar.h
+++ Robot/include/Robot/RoboCar.h
@@ -13,10 +13,12 @@
  * */
 class AccelerationSensor {
   public:
-  AccelerationSensor() {
+  AccelerationSensor() { // initialize with some default standard deviations
+    add_noise = true;
     distribution_gyro = std::normal_distribution<double>(0., 0.1);
     distribution_acc = std::normal_distribution<double>(0., 0.05);
-    add_noise = true;
+    std::random_device rd{};
+    gen = std::mt19937{rd()};
   }
   
   AccelerationSensor(const double sigma_gyro, const double sigma_acc) {
@@ -27,57 +29,66 @@ class AccelerationSensor {
   /**
    * \brief given the cars velocity vectors returns a sensor measurement for
    * the cars angular velocity
-   * \param [in] the 
-   *    and angular velocity of the car
+   * \param i_velocity [in] the current actual velocity vector of the car
    * \param [out] the angular velocity measured by the sensor
    */
   double gyroscopeMeasurement(const Vector3& i_velocity) {
-    // TODO implement noise
     if (add_noise) {
-      return i_velocity(2) + distribution_gyro(generator);
+      return i_velocity(2) + distribution_gyro(gen);
     } else {
       return i_velocity(2);
     }
   }
 
+  /**
+   * \brief computes the acceleration measurements of the sensor at the current
+   * timestep. The acceleration is made of a linear part and two angular parts:
+   *  a = linear_vel_car + psi x vector_to_point + omega x (omega x vector_to_point)
+   * \param i_velocity [in] the current actual velocity vector of the car
+   * \param [out] acceleration in intertial frame measured by the sensor
+   */
   Vector2 linearAccelerationsMeasurement(const Vector3& i_velocity,
                                          const Vector3& i_velocity_old,
                                          const Vector3& state,
                                          const double dt) {
+    // first simply get the linear part of the acceleration                                           
     Vector2 linear_part = (i_velocity.topRows(2) - i_velocity_old.topRows(2)) / dt;
     Vector2 acceleration = linear_part;
 
-    double angular_acceleration = (i_velocity(2) - i_velocity_old(2)) / dt;
-    Vector3 psi(0., 0., angular_acceleration);
+    double alpha = (i_velocity(2) - i_velocity_old(2)) / dt; // angular acceleration
+    // 3D vectors representing the rotation (acceleration and velocity)
+    Vector3 psi(0., 0., alpha);
     Vector3 omega(0., 0., i_velocity(2));
 
     // transformation matrix from car frame to inertial coordinate frame 
     Matrix3 I_C_T;
     I_C_T << cos(state(2)), -sin(state(2)), state(0),
               sin(state(2)), cos(state(2)), state(1),
-              0, 0, 1;
+              0,             0,             1;
     
     // the cars origin in inertial coordinate frame
     Vector3 I_car_origin = I_C_T * Vector3(0., 0., 0.);
 
     Vector3 angular_acc_part = psi.cross(I_car_origin);
     Vector3 angular_vel_part = omega.cross(omega.cross(I_car_origin));
-
+    // add the components together
     acceleration = acceleration + angular_acc_part.topRows(2) + 
                     angular_vel_part.topRows(2);
     if (add_noise) {
-      return acceleration + Vector2(distribution_acc(generator), distribution_acc(generator));
+      return acceleration + Vector2(distribution_acc(gen), distribution_acc(gen));
     } else {
       return acceleration;
     } 
   }
 
   private:
-  std::default_random_engine generator;
   std::normal_distribution<double> distribution_gyro;
   std::normal_distribution<double> distribution_acc;
+  std::mt19937 gen;
   bool add_noise;
 };
+
+
 
 class RoboCar {
   public:
@@ -111,6 +122,10 @@ class RoboCar {
    */
   void applyControlInput(Vector2 signal, double dt);
 
+  /**
+   * \brief updates the estimated states by only using inputs read from the IMU sensor
+   * \param dt [in] the timestep, that the car should be advanced
+   */
   void updateStateFromIMUMeasurement(const double dt);
 
   const Vector2 getPosition();
@@ -118,8 +133,8 @@ class RoboCar {
   const Vector3& getMeasuredState();
 
   Lidar lidar;
-  // width and length of the cars
-  const std::pair<double,double> CAR_DIMENSIONS = std::pair<double,double>(0.05, 0.25);
+  const std::pair<double,double> CAR_DIMENSIONS = 
+      std::pair<double,double>(0.05, 0.25); // width and length of the cars
 
   private:
   Vector2 signal; // input signal at current timestep
